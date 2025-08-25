fd0b08ebbe837766f7d771344f6a4b3c20e8b3b0
maehwa <68327208+maehwaaa@users.noreply.github.com>
2025-08-25 20:10:09 +0500

    Update #Задание 2.py

--- #Задание 2.py
+++ #Задание 2.py
@@ -1,6 +1,5 @@
-#Задание 2
-n = int(input("Введите кол-во чисел - "))
-arr = list(map(int, input("Введите числа через пробел - ").split()))
+n = int(input("Введите кол-во чисел (не меньше 4) - "))
+arr = list(map(int, input(f"Введите {n} чисел через пробел - ").split()))
 
 result = [arr[-1]] + arr[:-1]
 print("Результат -", *result)
